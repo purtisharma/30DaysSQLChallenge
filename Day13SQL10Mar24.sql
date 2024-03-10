5fcb908fe58334d08cd9d287266c66f039d5970c
Purti Sharma <56759052+purtisharma@users.noreply.github.com>
2024-03-10 08:10:49 +0000

    Day 13
    completed -Weather Observation Station 5

--- Day13SQL10Mar24.sql
+++ Day13SQL10Mar24.sql
@@ -35,3 +35,7 @@ WHERE ID % 2=0
 ----Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
 SELECT COUNT(CITY)- COUNT(DISTINCT CITY) AS DIFF
 FROM STATION
+
+---Weather Observation Station 5 Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+select CITY,LENGTH(CITY) from STATION order by Length(CITY) asc, CITY limit 1; 
+select CITY,LENGTH(CITY) from STATION order by Length(CITY) desc,CITY limit 1;
