6250ccde621b2f67cfe2c082142a72418d150041
Purti Sharma <56759052+purtisharma@users.noreply.github.com>
2024-03-16 21:32:58 +0000

    Day 19
    Tiny revision

---
+++ Day19SQL16Mar24.sql
@@ -0,0 +1,17 @@
+--Date 16 March Hackerrack- easy ones
+__-revision
+Select * 
+from dioptra.malawi_f;
+
+Select * 
+from dioptra.malawi_f;
+where Category='National Staff';\
+
+
+Select * 
+from dioptra.malawi_f;
+where Project is not null;
+
+update dioptra.malawi_f
+set Notes= 0000
+where BUAmount>=70000
